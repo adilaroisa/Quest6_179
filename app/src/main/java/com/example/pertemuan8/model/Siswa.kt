f46214c5f350ff30e73967cdd98d5597b0fec9e7
adilaroisa <adilaroisa688@gmail.com>
2025-11-21 10:15:45 +0700

    mengubah data class siswa

--- app/src/main/java/com/example/pertemuan8/model/Siswa.kt
+++ app/src/main/java/com/example/pertemuan8/model/Siswa.kt
@@ -1,7 +1,7 @@
 package com.example.pertemuan8.model
 
 data class Siswa(
-    val nama: String = "",
-    val gender: String = "",
-    val alamat: String = ""
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = ""
 )
