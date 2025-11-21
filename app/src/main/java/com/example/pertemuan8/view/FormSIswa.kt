bd0e433da54a64633d81d42606f3538dd99cfd93
adilaroisa <adilaroisa688@gmail.com>
2025-11-21 14:57:35 +0700

    mengubah dan menambahkan di form siswa bagian gender dan alamat

--- app/src/main/java/com/example/pertemuan8/view/FormSIswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/FormSIswa.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -83,10 +84,19 @@ fun FormIsian(
             )
             Row {
                 jenisK.forEach { item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+
+                        verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
-                            selected = false,
-                            onClick = { item }
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
                         Text(text = item)
                     }
@@ -100,17 +110,17 @@ fun FormIsian(
                 color = Color.Red
             )
             OutlinedTextField(
-                value = "",
+                value = txtAlamat,
                 singleLine = true,
                 modifier = Modifier
                     .width(250.dp),
                 label = {Text(text = "Alamat")},
-                onValueChange = {}
+                onValueChange = {txtAlamat = it}
             )
             Spacer(modifier = Modifier.height(30.dp))
             Button(
                 modifier = Modifier.fillMaxWidth(1f),
-                onClick = OnSubmitBtnClick
+                onClick ={OnSubmitBtnClick(listData)}
             ) {
                 Text(stringResource(R.string.submit))
             }
