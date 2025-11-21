4112167d95c11d348b91e679c04d32bb691ecb9f
adilaroisa <adilaroisa688@gmail.com>
2025-11-21 10:16:51 +0700

    membuat file navigasi

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/Navigasi.kt
@@ -1,2 +1,51 @@
 package com.example.pertemuan8.view.uicontroller
 
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                _root_ide_package_.com.example.pertemuan8.view.FormIsian(
+                    //pilihanJK = JenisJK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                _root_ide_package_.com.example.pertemuan8.view.TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
+}
